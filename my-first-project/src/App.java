5e65094a9b87b0a640b4c3bd0d7b9bae40b41c73
Cristiano Cruz <cruzcristianno@gmail.com>
2023-08-31 11:24:04 -0300

    commit2

--- my-first-project/src/App.java
+++ my-first-project/src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /*boletim de aluno */
+        Scanner sc = new Scanner(System.in);
+            System.out.println("digite a nota");
+            
+        int mediaFinal = sc.nextInt();
+        
+        if(mediaFinal<6)
+            System.out.println("REPROVADO");
+        else if(mediaFinal==6)
+            System.out.println("RECUPERAÇÂO");
+        else
+            System.out.println("APROVADO");
+            sc.close();
     }
 }
+
